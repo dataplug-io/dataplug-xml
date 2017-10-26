fd75680cf745888a045e339b3fca12f1bcba4600
Alexey Pelykh <alexey.pelykh@gmail.com>
2017-10-26 17:15:21 +0300

    Add support for 'complete' event

--- test/xmlStreamReader.spec.js
+++ test/xmlStreamReader.spec.js
@@ -17,4 +17,14 @@ describe('XmlStreamReader', () => {
     reader.read()
       .should.be.deep.equal(object)
   })
+
+  it('supports "complete" event', (done) => {
+    const reader = new XmlStreamReader('/*/item')
+    reader.on('complete', (data) => {
+      data.should.be.equal('<root><notItem/></root>')
+      done()
+    })
+    reader.write('<?xml version="1.0" encoding="UTF-8"?><root><notItem/><item attr="attrValue"><subObject attr="attrValue"/></item></root>')
+    reader.end()
+  })
 })
